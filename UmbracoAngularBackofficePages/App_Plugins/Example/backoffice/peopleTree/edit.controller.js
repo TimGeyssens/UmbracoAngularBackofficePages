d30899044ef3d0c0cfc6a82cc4e646e94f2034f0
TimGeyssens <tg@umbraco.com>
2014-08-16 15:10:04 +0200

    Finish example, create now also working

--- UmbracoAngularBackofficePages/App_Plugins/Example/backoffice/peopleTree/edit.controller.js
+++ UmbracoAngularBackofficePages/App_Plugins/Example/backoffice/peopleTree/edit.controller.js
@@ -1,10 +1,15 @@
 ﻿angular.module("umbraco").controller("People.PersonEditController",
 	function ($scope, $routeParams, personResource, notificationsService) {
 
+	    if ($routeParams.id == -1) {
+	        $scope.person = {};
+	    }
+        else{
 	    //get a person id -> service
 	    personResource.getById($routeParams.id).then(function (response) {
 	        $scope.person = response.data;
 	    });
+	    }
 
 	    $scope.save = function (person) {
 	        personResource.save(person).then(function (response) {
