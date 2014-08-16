3006a90fd0d51daca6c7cc591a1480ac46226b9b
TimGeyssens <tg@umbraco.com>
2014-08-16 14:21:36 +0200

    Initial commit

---
+++ UmbracoAngularBackofficePages/Person.cs
@@ -0,0 +1,21 @@
+﻿namespace UmbracoAngularBackofficePages
+{
+    public class Person
+    {
+        public Person(int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
---
+++ UmbracoAngularBackofficePages/Trees/PeopleTreeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Web.Trees;
+
+namespace UmbracoAngularBackofficePages.Trees
+{
+    public class PeopleTreeController: TreeController
+    {
+        protected override Umbraco.Web.Models.Trees.MenuItemCollection GetMenuForNode(string id, System.Net.Http.Formatting.FormDataCollection queryStrings)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Umbraco.Web.Models.Trees.TreeNodeCollection GetTreeNodes(string id, System.Net.Http.Formatting.FormDataCollection queryStrings)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
