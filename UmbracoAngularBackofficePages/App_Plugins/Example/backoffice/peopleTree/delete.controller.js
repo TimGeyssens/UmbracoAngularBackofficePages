1f6b422395dbe22503cc0c6701637fa3a47f415f
TimGeyssens <tg@umbraco.com>
2014-08-17 13:14:12 +0200

    Adds syncTree to correct file

--- UmbracoAngularBackofficePages/App_Plugins/Example/backoffice/peopleTree/delete.controller.js
+++ UmbracoAngularBackofficePages/App_Plugins/Example/backoffice/peopleTree/delete.controller.js
@@ -4,10 +4,11 @@
 	    $scope.delete = function (id) {
 	        personResource.deleteById(id).then(function () {
 	            navigationService.hideNavigation();
+	            navigationService.syncTree({ tree: 'peopleTree', path: ["-1", $scope.currentNode.id], forceReload: true });
 	        });
 
 	    };
 	    $scope.cancelDelete = function () {
-	       navigationService.hideNavigation();
+	        navigationService.hideNavigation();
 	    };
 	});
