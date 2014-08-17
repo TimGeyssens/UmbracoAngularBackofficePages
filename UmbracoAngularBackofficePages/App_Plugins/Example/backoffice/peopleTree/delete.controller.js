7339753ab758aa70a9d5621c1abd1d11472074bf
TimGeyssens <tg@umbraco.com>
2014-08-17 13:27:22 +0200

    Tree syncing not working yet

--- UmbracoAngularBackofficePages/App_Plugins/Example/backoffice/peopleTree/delete.controller.js
+++ UmbracoAngularBackofficePages/App_Plugins/Example/backoffice/peopleTree/delete.controller.js
@@ -4,7 +4,7 @@
 	    $scope.delete = function (id) {
 	        personResource.deleteById(id).then(function () {
 	            navigationService.hideNavigation();
-	            navigationService.syncTree({ tree: 'peopleTree', path: ["-1", $scope.currentNode.id], forceReload: true });
+	            //navigationService.syncTree({ tree: 'peopleTree', path: ["-1", $scope.currentNode.id], forceReload: true });
 	        });
 
 	    };
